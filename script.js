580d386dc0f55d08d390e5ef85791bb71ffe5560
Joe Cottam <joe@joecottam.com>
2018-11-06 23:40:58 +0000

    created js file and created 4 simple +-*/ functions

---
+++ script.js
@@ -0,0 +1,24 @@
+function add(a, b) {
+    return a + b
+}
+
+function substract(a, b) {
+    return a - b
+}
+function multiply(a, b) {
+    return a * b
+}
+function divide(a, b) {
+    return (b == 0) ? "Cannot divide by zero" : a / b;
+}
+// function operate(operator, a, b) {
+
+// }
+// buttons = document.querySelectorAll('button');
+// displayPara = document.querySelector('p');
+// buttons.forEach((button) => {
+//     button.addEventListener('click', (e) => {
+//         displayPara.textContent = displayPara.textContent + (button.textContent);
+//     });
+
+// });
