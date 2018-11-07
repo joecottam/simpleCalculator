72b5b127118d3c8db7bab4f9f27336a5a46ffa87
Joe Cottam <joe@joecottam.com>
2018-11-07 21:20:44 +0000

    added classes to buttons, basic calculator function working for each operator and clear button working

--- script.js
+++ script.js
@@ -1,24 +1,70 @@
 function add(a, b) {
-    return a + b
+    return Number(a) + Number(b)
 }
 
 function substract(a, b) {
-    return a - b
+    return Number(a) - Number(b)
 }
 function multiply(a, b) {
-    return a * b
+    return Number(a) * Number(b)
 }
 function divide(a, b) {
-    return (b == 0) ? "Cannot divide by zero" : a / b;
+    return (Number(b) == 0) ? "Cannot divide by zero" : Number(a) / Number(b);
 }
-// function operate(operator, a, b) {
+function operate(operator, a, b) {
+    if (operator == 'button_addition') {
+        return add(a, b);
+    } else if (operator == 'button_substraction') {
+        return substract(a, b);
+    } else if (operator == 'button_multiply') {
+        return multiply(a, b);
+    } else if (operator == 'button_divide') {
+        return divide(a, b);
+    }
 
-// }
-// buttons = document.querySelectorAll('button');
-// displayPara = document.querySelector('p');
-// buttons.forEach((button) => {
-//     button.addEventListener('click', (e) => {
-//         displayPara.textContent = displayPara.textContent + (button.textContent);
-//     });
+}
+let firstNumber;
+let secondNumber;
+let operatorString;
+let numberButtons = document.querySelectorAll('.number_buttons');
+let displayPara = document.querySelector('p');
 
-// });
+numberButtons.forEach((numberButton) => {
+    numberButton.addEventListener('click', () => {
+        if (displayPara.textContent == '0') {
+            clearDisplay();
+        }
+        displayPara.textContent += (numberButton.textContent);
+    });
+
+});
+
+function defaultDisplay() {
+    displayPara.textContent = "0";
+}
+
+function clearDisplay() {
+    displayPara.textContent = "";
+}
+
+let clearButton = document.querySelector('#button_clear');
+clearButton.addEventListener('click', () => {
+    defaultDisplay();
+});
+
+let operatorButtons = document.querySelectorAll('.operator_buttons')
+operatorButtons.forEach((operatorButton) => {
+    operatorButton.addEventListener('click', () => {
+        operatorString = operatorButton.id;
+        firstNumber = displayPara.textContent;
+        defaultDisplay();
+    });
+});
+
+let equalsButton = document.querySelector('#button_equals');
+equalsButton.addEventListener('click', () => {
+    secondNumber = displayPara.textContent;
+    displayPara.textContent = operate(operatorString, firstNumber, secondNumber);
+
+});
+
